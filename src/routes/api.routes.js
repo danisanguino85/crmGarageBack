8a0b49285714d7e2d7b0f90904b8947277e04bc1
Jean Franco ravelo <jeanfrancoravelo@gmail.com>
2025-04-10 17:37:01 +0200

    recuperacion de datos del usuario, pendiente crear el usuario

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -4,5 +4,8 @@ router.use("/reparaciones", require("./api/reparaciones.routes"));
 router.use("/usuarios", require("./api/usuarios.routes"));
 router.use("/vehiculos", require("./api/vehiculos.routes"));
 router.use("/clientes", require("../routes/api/clientes.routes"));
+router.use("/usuarios/id", require("./api/usuarios.routes"));
+router.use("/usuarios/telefono", require("./api/usuarios.routes"));
+router.use("/usuarios/email", require("./api/usuarios.routes"));
 
 module.exports = router;
