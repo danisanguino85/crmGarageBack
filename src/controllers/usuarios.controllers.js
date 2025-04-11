45bb6d664589cbeb18f03647dee58308d2873124
Jean Franco ravelo <jeanfrancoravelo@gmail.com>
2025-04-11 09:12:40 +0200

    hice la peticion de crear y actualizar el usuario

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -1,50 +1,77 @@
 const usuariosModel = require("../models/usuarios.models");
 
 const getAllUsuarios = async (req, res, next) => {
-	try {
-		const usuarios = await usuariosModel.selectAllUsuarios();
+    try {
+        const usuarios = await usuariosModel.selectAllUsuarios();
 
-		res.json(usuarios);
-	} catch (error) {
-		next(error);
-	}
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getUsuariosById = async (req, res, next) => {
-	const { id } = req.body;
-	try {
-		const usuarios = await usuariosModel.selectAllUsuariosById(id);
-		res.json(usuarios);
-	} catch (error) {
-		next(error);
-	}
+    const { id } = req.params;
+    try {
+        const usuarios = await usuariosModel.selectAllUsuariosById(id);
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getUsuariosTelefono = async (req, res, next) => {
-	const { telefono } = req.body;
+    const { telefono } = req.body;
 
-	try {
-		const usuarios = await usuariosModel.selectAllUsuariosByTelefono(telefono);
-		res.json(usuarios);
-	} catch (error) {
-		next(error);
-	}
+    try {
+        const usuarios =
+            await usuariosModel.selectAllUsuariosByTelefono(telefono);
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getUsuariosByEmail = async (req, res, next) => {
-	const { email } = req.body;
+    const { email } = req.body;
 
-	try {
-		const usuarios = await usuariosModel.selectAllUsuariosByEmail(email);
-		res.json(usuarios);
-	} catch (error) {
-		next(error);
-	}
+    try {
+        const usuarios = await usuariosModel.selectAllUsuariosByEmail(email);
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const createUsuario = async (req, res, next) => {
+    try {
+        const result = await usuariosModel.selectAllCrearUsuario(req.body);
+        const usuarios = await usuariosModel.selectAllUsuariosById(
+            result.insertId,
+        );
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
+};
+
+const updateUsuario = async (req, res, next) => {
+    const { id } = req.params;
+
+    try {
+        const result = await usuariosModel.selectAllupdateById(id, req.body);
+        const usuarios = await usuariosModel.selectAllUsuariosById(id);
+        res.json(usuarios);
+    } catch (error) {
+        next(error);
+    }
 };
 
 module.exports = {
-	getAllUsuarios,
-	getUsuariosById,
-	getUsuariosTelefono,
-	getUsuariosByEmail,
+    getAllUsuarios,
+    getUsuariosById,
+    getUsuariosTelefono,
+    getUsuariosByEmail,
+    createUsuario,
+    updateUsuario,
 };
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 
 router.use("/reparaciones", require("./api/reparaciones.routes"));
-router.use("/usuarios", require("./api/usuarios.routes"));
 router.use("/vehiculos", require("./api/vehiculos.routes"));
 router.use("/clientes", require("../routes/api/clientes.routes"));
-router.use("/usuarios/id", require("./api/usuarios.routes"));
+router.use("/usuarios/:id", require("./api/usuarios.routes"));
 router.use("/usuarios/telefono", require("./api/usuarios.routes"));
 router.use("/usuarios/email", require("./api/usuarios.routes"));
+router.use("/usuarios", require("./api/usuarios.routes"));
 
 module.exports = router;
